5eb6bc7bd7539db6a4d7fcf24a04ab202f5f41fd
imgrizi <brunomiguel.lopessilva@gmail.com>
2026-01-06 15:58:48 +0000

    Js trainning

---
+++ JavaScript/console_log.js
@@ -0,0 +1,5 @@
+console.log(6);
+console.log(25);
+console.log(7);
+console.log(25.2);
+console.log("PLUS ULTRA");
---
+++ JavaScript/variaveis.js
@@ -0,0 +1,15 @@
+// let
+let age;
+console.log(age);
+
+let age2 =25;
+console.log(age2);
+
+age2 =50;
+console.log(age2);
+
+// const
+const maxScore=999;
+console.log(maxScore);
+
+maxScore =1000;//error
