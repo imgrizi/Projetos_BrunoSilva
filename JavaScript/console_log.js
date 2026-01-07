7e7fb1e9d7ae0757da2202b18e1ba9757b9ef7e5
imgrizi <brunomiguel.lopessilva@gmail.com>
2026-01-07 11:37:00 +0000

    js trainning day 2

--- JavaScript/console_log.js
+++ JavaScript/console_log.js
@@ -3,3 +3,9 @@ console.log(25);
 console.log(7);
 console.log(25.2);
 console.log("PLUS ULTRA");
+
+let userName= "Bruno";
+console.log(`Hello, ${userName}!`);
+
+let age= 27;
+console.log(`Name:${userName}, Age:${age}`)
---
+++ JavaScript/string.js
@@ -0,0 +1,19 @@
+let firstQuote = "First String";
+console.log(firstQuote);
+let secondQuote = "Second Quote";
+console.log(secondQuote);
+
+
+let firstName = "Bruno";
+let secondName = "Silva";
+let fullName = firstName + " " +secondName;
+console.log(fullName);
+
+let greeting = "Welcome";
+greeting += ", Bruno";
+console.log(greeting);
+
+let goodbye = "Goodbye,";
+goodbye = `${goodbye} ${firstName}`;
+console.log(goodbye);
+
