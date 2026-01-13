abd1b03c15e0347917365d091dd3c6b2a2170dc1
imgrizi <brunomiguel.lopessilva@gmail.com>
2026-01-13 15:13:19 +0000

    js trainning day4

---
+++ JavaScript/triviaBot.js
@@ -0,0 +1,16 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "astroBot";
+let botLocation = "neverland";
+let favoriteLanguage = "HTML5";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is a markup language used for structuring and presenting hypertext documents on the World Wide Web.";
+console.log(codingFact);
+codingFact = favoriteLanguage + " is the fifth version of Hypertext Markup Language (HTML).";
+console.log(codingFact);
+codingFact = favoriteLanguage +  " was designed so browsers could play video and audio without plugins like Flash.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + "." );
