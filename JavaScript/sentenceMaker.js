9527414b01fc25de45279bfe9c958019edc544ed
imgrizi <brunomiguel.lopessilva@gmail.com>
2026-01-14 13:36:40 +0000

    js trainning day5

---
+++ JavaScript/sentenceMaker.js
@@ -0,0 +1,19 @@
+let adjective = "giant";
+let noun = "dragon";
+let verb = "furious";
+let place = "neverland";
+let adjective2 = "big";
+let noun2 = "lasagna";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective +" "+ noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log( "First story: " + firstStory);
+
+ adjective = "small";
+ noun = "crocodile";
+ verb = "happy";
+ place = "atlantis";
+ adjective2 = "cute";
+ noun2 = "tuna";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective +" "+ noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log( "Second story: " + secondStory);
